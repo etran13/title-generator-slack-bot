d474632daad40580214c052d5d78caf983e7ef11
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-05-08 16:03:39 -0500

    Message sent successfully

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 from urllib.error import HTTPError, URLError
 from urllib.request import urlopen, Request
+import json
 
-def make_request(url, headers=None, data=None):
-    request = Request(url, headers=headers or {}, data=data) #Create a request object w/ url
-    try:
-        with urlopen(request, timeout=10) as response: #Get the HTTP response
-            print(response.status)
-            return response.read(), response
-    except HTTPError as error:
-        print(error.status, error.reason)
-    except URLError as error:
-        print(error.reason)
-    except TimeoutError:
-        print("Request timed out")
+url = "https://slack.com/api/chat.postMessage"
+
+headers = {
+    "Authorization": "Bearer xoxb-REDACTED",
+    "Content-Type": "application/json; charset=utf-8"
+}
+
+data = json.dumps({
+    "channel": "#ellies-wf-slackbot-test",
+    "text": "Hello from PYTHON!"
+}).encode("utf-8") #Encode data as a JSON object
+
+request = Request(url, data, headers)
+
+response = urlopen(request)
+print(json.loads(response.read()))
